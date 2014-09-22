c2dd0f29d75deb8e2a7aa993f216682aec70c55b
Joel Griffith <jgriffith@appnexus.com>
2014-09-22 13:53:50 -0700

    Implements CSS Build. #CSSBuild

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ var gulp = require('gulp');
 var webpackConfig = require('./webpack.conf.js');
 var webpack = require('gulp-webpack');
 var uglify = require('gulp-uglify');
+var autoprefixer = require('gulp-autoprefixer');
+var sass = require('gulp-sass');
+var minifycss = require('gulp-minify-css');
 
 // JS packaging for distribution/dev/test
 gulp.task('js', function() {
@@ -10,4 +13,16 @@ gulp.task('js', function() {
 		.pipe(gulp.dest('build/dev/js'))
 		.pipe(uglify())
 		.pipe(gulp.dest('build/dist/js'));
+});
+
+// Stylesheets
+gulp.task('css', function() {
+	return gulp.src('src/css/index.scss')
+		.pipe(sass())
+		.pipe(autoprefixer({
+			browsers: ['last 5 versions']
+		}))
+		.pipe(gulp.dest('build/dev/css'))
+		.pipe(minifycss())
+		.pipe(gulp.dest('build/dist/css'));
 });
